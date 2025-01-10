eb0638c8ac6b95429a7659b49669eec0a25cb58e
Guerola <antonioguerolacrespo@gmail.com>
2025-01-10 11:23:10 +0100

    Error 404 Page 50%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HelloWorldComponent } from './pages/hello-world/hello-world.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'hola-mundo', component: HelloWorldComponent }
+    { path: 'hola-mundo', component: HelloWorldComponent },
+    { path: '**', component: Error404Component }
 ];
