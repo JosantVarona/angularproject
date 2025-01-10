c032f1904e1336f984c24a4e0669284b9384674d
JosantVarona <jvaronarubio@gmail.com>
2025-01-10 13:37:24 +0100

    completacion de firestore en grupo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+    firestore: {
+        projectId: 'angular3d-fe208',
+        appId: '1:187804397696:web:45750e8702a2f6422a7665',
+        storageBucket: 'angular3d-fe208.firebasestorage.app',
+        apiKey: 'AIzaSyD5P5RObHKhBz3ZLyCZspfxe3fXwre6LvM',
+        authDomain: 'angular3d-fe208.firebaseapp.com',
+        messagingSenderId: '187804397696',
+      },
+};
