cf9990b872d821d8d4bc5fa5ff3cc0656641916c
JosantVarona <jvaronarubio@gmail.com>
2025-01-11 15:08:52 +0100

    Autentificaion Google

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,13 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.development';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp(environment.firestore)), provideFirestore(() => getFirestore())]
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideFirebaseApp(() => initializeApp(environment.firestore)),
+    provideFirestore(() => getFirestore()), 
+    provideAuth(() => getAuth())]
 };
