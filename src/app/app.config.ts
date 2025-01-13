2370aff3bacd2708771cfdcf72c61669bf8f26e9
Guerola <antonioguerolacrespo@gmail.com>
2025-01-13 13:07:43 +0100

    Correciones varias

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.development';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    provideClientHydration(), 
     provideFirebaseApp(() => initializeApp(environment.firestore)),
     provideFirestore(() => getFirestore()), 
-    provideAuth(() => getAuth())]
+    provideAuth(() => getAuth())
+  ],
 };
